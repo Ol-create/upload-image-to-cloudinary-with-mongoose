6cbebef34359cf2c3d544dad36f712acb3485f6b
Ol-create <oluola96@gmail.com>
2023-10-09 19:13:01 +0100

    add dotenv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const imageRouter = require("./routes/upload")
+require("dotenv").config();
 
 const app = express()
 app.use("/api/images", imageRouter);
